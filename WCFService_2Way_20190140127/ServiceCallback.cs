e26802714a6a73e955fe30577b0e3de94678191a
Nadziraraa <nadziraazahra10@gmail.com>
2022-01-10 15:01:09 +0700

    Add project files.

---
+++ WCFService_2Way_20190140127/ServiceCallback.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_20190140127
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ServiceCallback : IServiceCallback
+    {
+        Dictionary<IClientCallback, string> userList = new Dictionary<IClientCallback, string>(); //menyimpan user data online
+        public void gabung(string username)
+        {
+            IClientCallback koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallback>(); //untuk menampung user ketika baru daftar/buat akun
+            userList[koneksiGabung] = username;
+        }
+
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            IClientCallback koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallback>(); // mnegirim data user dan pesan ke user lain
+            string user;
+            if (!userList.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach (IClientCallback other in userList.Keys)
+            {
+                other.pesanKirim(user, pesan);
+            }
+        }
+    }
+}
